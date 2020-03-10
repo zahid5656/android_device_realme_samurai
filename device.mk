714434966fb7087113760400b91a94054fba5eaf
Jaegeuk Kim <jaegeuk@google.com>
2020-03-10 11:08:34 -0700

    samurai: use metadata_csum for /metadata
    This enhances ext4 stability.
    
    Bug: 149391799
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: I8cebe30bdf66f39f0e4cc5955e53aca4d4f22f64

--- device.mk
+++ device.mk
@@ -278,6 +278,11 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+PRODUCT_PACKAGES += \
+    e2fsck_ramdisk \
+    tune2fs_ramdisk \
+    resize2fs_ramdisk
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
