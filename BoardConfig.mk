557d3d084edcb531cd5a1d0916f76a75e6be06e5
Georg Veichtlbauer <georg@vware.at>
2024-02-27 08:52:31 +0100

    samurai: Drop DT2W node from BoardConfig
    Not needed after 917d9374.
    
    Change-Id: I926ee3a53e1ae8141bef3c214de9a96e227777a2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
