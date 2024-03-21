0aebbc9b81b90f91e5d3caa677316c80ae168f62
Bruno Martins <bgcngm@gmail.com>
2024-03-21 16:31:24 +0000

    samurai: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- device.mk
+++ device.mk
@@ -436,10 +436,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samurai
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
